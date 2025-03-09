c90c8104717733af28f2fc0f69accd81ea884bc1
naime-hossain <hossain.naime@yahoo.com>
2025-03-09 21:47:08 +0600

    Update: single page ui uodate

--- src/pages/blog/[title].vue
+++ src/pages/blog/[title].vue
@@ -1,15 +1,42 @@
 <template>
-  <div v-if="post">
-    <h1>{{ post.title }}</h1>
-    <p><strong>Author:</strong> {{ post.author }} || Date : {{ post.date }}</p>
-    <p>{{ post.text }}</p>
-    <router-link to="/">Back to Blogs</router-link>
-  </div>
-  <div v-else>
-    <p>Post not found.</p>
-    {{ route.params.title }}
-    <router-link to="/">Go back</router-link>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card v-if="post" class="pa-4">
+          <v-card-title class="text-h5 font-weight-bold">
+            {{ post.title }}
+          </v-card-title>
+
+          <v-card-subtitle class="text-body-2 text-grey-darken-1">
+            <strong>Author:</strong> {{ post.author }} | <strong>Date:</strong>
+            {{ post.date }}
+          </v-card-subtitle>
+
+          <v-card-text class="mt-3 text-body-1">
+            {{ post.text }}
+          </v-card-text>
+
+          <v-divider class="my-4"></v-divider>
+
+          <v-card-actions>
+            <v-btn to="/" variant="tonal" color="primary">
+              <v-icon start>mdi-arrow-left</v-icon>
+              Back to Blogs
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+
+        <v-alert v-else type="error" prominent class="mt-4">
+          <v-alert-title>Post Not Found</v-alert-title>
+          <p>The post with title "{{ route.params.title }}" does not exist.</p>
+          <v-btn to="/" color="primary" variant="tonal">
+            <v-icon start>mdi-arrow-left</v-icon>
+            Go Back
+          </v-btn>
+        </v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
